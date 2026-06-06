5988d8f272beefb6141536ef7ea816f199a1d3db
Muhammad Suffiyan Rafi <56222285+SufyanCh632@users.noreply.github.com>
2026-06-06 15:57:39 +0500

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,26 @@
+var eng = 85;
+let math = 70;
+
+const phy = 65;
+
+eng = 85 + 5;
+math = 70 - 5;
+
+
+console.log(eng);
+console.log(math);
+console.log(phy);
+
+var stdName = "Ahmed"; 
+var mulString = `
+I love Pakistan
+My army is our great
+Imran Khan is our Leader
+Lets play soccer`
+
+var fName = "Ahmed";
+var lName = "Khan";
+
+console.log(stdName);
+console.log(mulString);
+console.log(fName +" "+ lName);
